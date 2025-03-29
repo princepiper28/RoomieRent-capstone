d3dda553d84476322acc4a4bd8c0ff369ea0dbc2
prince joseph udoewah <princepiper28@gmail.com>
2025-03-30 00:41:22 +0100

    Prepare for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { HashRouter } from "react-router-dom";  // ✅ Use HashRouter for GitHub Pages
 import ReactDOM from 'react-dom/client';
 import './index.css'; 
-import App from './App';   // <-- Check that this is correctly written
+import App from './App';   
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>  {/* ✅ Wrapped App inside HashRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
